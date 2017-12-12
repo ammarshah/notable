f49e69004cc2a188c3d404cc21359899ab461f04
Ammar Shah <ammarshah123@gmail.com>
2017-12-12 18:14:29 +0500

    Update a Note API

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -29,6 +29,21 @@ module.exports = function(app, db) {
   	});
   });
 
+  // Update a Note
+  app.put('/notes/:id', (req, res) => {
+  	const id = req.params.id; // ID as a string from URL
+  	const details = { '_id': new ObjectID(id) }; // Mongo requires _id as an ObjectID to find its related object
+  	const note = { title: req.body.title, text: req.body.body }; // Create a JSON object
+
+  	db.collection('notes').update(details, note, (err, item) => {
+  		if(err) {
+  			res.send({ 'error': 'An error has occured' });
+  		} else {
+  			res.send(item);
+  		}
+  	});
+  });
+
   // Delete a Note
   app.delete('/notes/:id', (req, res) => {
   	const id = req.params.id; // ID as a string from URL
@@ -38,7 +53,7 @@ module.exports = function(app, db) {
   		if(err) {
   			res.send({ 'error': 'An error has occured' });
   		} else {
-  			res.send('Note ' + id + 'deleted!');
+  			res.send('Note ' + id + ' deleted!');
   		}
   	});
   });
